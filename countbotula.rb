c7192f8f0fcb5890e794d736a19ea004dcf6aa3e
Elliot Wesoff <elliot@techrockstars.com>
2014-06-09 19:35:39 -0700

    added google graph

--- countbotula.rb
+++ countbotula.rb
@@ -1,35 +1,56 @@
 require 'rubygems'
 require "awesome_print"
 require 'csv'
+require 'googlecharts'
+
+puts "Enter Client Name:"
+client_name = gets
+current_time = Time.now.strftime("%m-%d-%Y")
+
 
 # {:computer_name => "Operations-03"}
 # import csv file
 
-#def import_data
   blocked_dns = []
   
   CSV.foreach('opendnsfile.csv') do |row|
-    ap row
+    # ap row
     blocked_dns << row[2]
 
   end
   # Delete the Header from array
   blocked_dns.delete_at(0)
+  
   # Put all the comptuers into an array and make it unique
   unique_computers = blocked_dns.uniq
   
-# end
 
-# counts each blocked dns requtes for each computer
-dirty_computer = unique_computers.map {|computer| {computer => blocked_dns.count(computer)}}
+  # counts each blocked dns requtes for each computer
+  dirty_computer = unique_computers.map {|computer| {computer => blocked_dns.count(computer)}}
 
-# opendns_data = [{}]
+  # This will save to file
+  # CSV.open("#{client_name}-#{current_time}.csv", 'w') do |csv_object|
+  #     csv_object << dirty_computer.map{|computer| computer.keys.first}
+  #     csv_object << dirty_computer.map{|computer| computer.values.first}
+  #   end
 
-CSV.open('dirty_dirty_computers.csv', 'w') do |csv_object|
-    csv_object << dirty_computer.map{|computer| computer.keys.first}
-    csv_object << dirty_computer.map{|computer| computer.values.first}
-  end
+  ap dirty_computer
+  ap dirty_computer.map{|computer| [computer.values.first]}
+  ap dirty_computer.map{|computer| [computer.keys.first]}
+  
 
+  chart = Gchart.new( :type => 'bar',
+                      :title => "#{client_name} - Number of Blocked Malware Requests #{current_time}",
+                      :theme => :keynote,
+                      :data => dirty_computer.map{|computer| computer.values.first}, 
+                      :line_colors => 'e0440e,e62ae5,287eec',
+                      :legend => dirty_computer.map{|computer| [computer.keys.first]},
+                      :labels => dirty_computer.map{|computer| [computer.keys.first]},
+                      :axis_with_labels => ['x', 'y'], 
+  					          :stacked => false,
+                      :filename => "chart.png",
+                      :size => "700x500")
+                      
+  # Record file in filesystem
+  chart.file
 
-
-#import_data
